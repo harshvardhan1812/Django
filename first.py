57ee1f902a4e7600a0aa5ce60ac1f675c7f808af
harshvardhan1812 <109271992+harshvardhan1812@users.noreply.github.com>
2024-11-10 02:03:37 +0530

    first Tutorial

--- first.py
+++ first.py
@@ -1,2 +1,5 @@
-print("hello this is going to be my first Django program")
-print("I am going to make changes and then raise a PR because I am making this change in the feature branch not in the main branch")
+# print("hello this is going to be my first Django program")
+# print("I am going to make changes and then raise a PR because I am making this change in the feature branch not in the main branch")
+import django
+
+print(django.get_version())
